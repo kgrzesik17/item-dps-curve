00498f00d10e7dbf1279251a8df0317816ac5395
Kacper <kgrzesik17@gmail.com>
2025-07-15 23:10:29 +0200

    on hit damage calculation

--- main.py
+++ main.py
@@ -1,10 +1,28 @@
-import matplotlib.pyplot as plt
-import numpy as np
+# import matplotlib.pyplot as plt
+# import numpy as np
+import random
 
+ad = 100
+attack_speed = 1.0
+crit_chance = 50
+crit_damage = 1.75  # 2.15 with IE 
 
+def is_crit(crit_chance):
+        if random.randrange(0, 100) < crit_chance:
+             return True
+        else: 
+             return False
+        
 
-xpoints = np.array([1, 3, 1, 8])
-ypoints = np.array([3, 10, -2, 14])
+def hit(ad, crit_chance, crit_damage):
+    if is_crit(crit_chance):
+          return ad * crit_damage
+    else:
+          return ad 
 
-plt.plot(xpoints, ypoints)
-plt.show()
+print(hit(ad, crit_chance, crit_damage))
+# xpoints = np.array([1, 3, 1, 8])
+# ypoints = np.array([3, 10, -2, 14])
+
+# plt.plot(xpoints, ypoints)
+# plt.show()
