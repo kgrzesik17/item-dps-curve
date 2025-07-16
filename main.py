88f632d8b121adb424d7d21804db58183c4c40cb
Kacper <kgrzesik17@gmail.com>
2025-07-16 15:32:04 +0200

    added item dps calculations

--- main.py
+++ main.py
@@ -6,47 +6,48 @@ from Item import *
 from items import *
 
 dps_list = []
-items_to_check = [berserkers_greaves, rapid_firecannon, yun_tal_wildarrows, lord_dominiks_regards, infinity_edge]
+items_to_check = [berserkers_greaves, yun_tal_wildarrows, infinity_edge, lord_dominiks_regards, rapid_firecannon]
+
+items_to_check.append('')
+current_items = []
 
 for i in items_to_check:
-    current_items = []
+    caitlyn = Champion("Caitlyn", 18, current_items, 27, 4.7, 30, 1.3, 60, 3.8, 1.75, 0.681, 0.04, 0.625)
 
+    ad = caitlyn.ad
+    attack_speed = caitlyn.attack_speed
+    crit_chance = caitlyn.crit_chance
+    crit_damage = caitlyn.crit_damage
 
-caitlyn = Champion("Caitlyn", 18, [], 27, 4.7, 30, 1.3, 60, 3.8, 1.75, 0.681, 0.04, 0.625)
+    def is_crit(crit_chance):
+            if random.randrange(0, 100) < crit_chance:
+                return True
+            else: 
+                return False
+            
 
-print(caitlyn.crit_damage)
+    def hit(ad, crit_chance, crit_damage):
+        if is_crit(crit_chance):
+            return ad * crit_damage
+        else:
+            return ad 
 
-ad = caitlyn.ad
-attack_speed = caitlyn.attack_speed
-crit_chance = caitlyn.crit_chance
-crit_damage = caitlyn.crit_damage
+    def average_damage(ad, crit_chance, crit_damage, amount = 5000):
+        full = 0
 
-def is_crit(crit_chance):
-        if random.randrange(0, 100) < crit_chance:
-             return True
-        else: 
-             return False
-        
+        for i in range(amount):
+            full += hit(ad, crit_chance, crit_damage)
 
-def hit(ad, crit_chance, crit_damage):
-    if is_crit(crit_chance):
-          return ad * crit_damage
-    else:
-          return ad 
+        output = full / amount
 
-def average_damage(ad, crit_chance, crit_damage, amount = 5000):
-    full = 0
+        return output
 
-    for i in range(amount):
-        full += hit(ad, crit_chance, crit_damage)
+    dps = average_damage(ad, crit_chance, crit_damage) * attack_speed
 
-    output = full / amount
+    current_items.append(i)
+    dps_list.append(round(dps))
 
-    return output
-
-dps = average_damage(ad, crit_chance, crit_damage) * attack_speed
-
-print(dps)
+print(dps_list)
 
 # xpoints = np.array([1, 3, 1, 8])
 # ypoints = np.array([3, 10, -2, 14])
